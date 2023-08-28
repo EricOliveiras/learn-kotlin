127ce29ee441f8cc252c6c02bf63c17303c63b57
Eric Oliveira <oiericoi@hotmail.com>
2023-08-28 12:49:43 -0300

    feat: add kotlin when control flow examples

---
+++ src/main/kotlin/controlFlow/when.kt
@@ -0,0 +1,28 @@
+package controlFlow
+
+fun main() {
+    cases(0L)
+    println(whenAssign(1))
+}
+
+fun cases(obj: Any) {
+    when(obj) {
+        1 -> println("One")
+        "Hello" -> println("Greeting")
+        is Long -> println("Long")
+        !is String -> println("Not a string")
+        else -> println("Unknown")
+    }
+}
+
+fun whenAssign(obj: Any): Any {
+    val result = when(obj) {
+        1 -> "one"
+        "Hello" -> 1
+        is Long -> false
+        else -> 42
+    }
+    return result
+}
+
+class MyClass
