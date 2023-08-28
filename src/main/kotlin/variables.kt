da911bd1d96e83e78eec9cd7be5046fa6f5f664d
Eric Oliveira <oiericoi@hotmail.com>
2023-08-28 08:57:03 -0300

    feat: add kotlin variables examples

---
+++ src/main/kotlin/variables.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // var é para variaveis mutaveis
+    var a: String = "Initial"
+    println(a)
+
+    // val é para variaveis imutaveis
+    val b: Int = 1
+    val c = 3
+    println(b)
+    println(c)
+}
